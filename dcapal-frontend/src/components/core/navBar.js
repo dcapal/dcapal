6fcb4d72f7c75e854c03a9cb0e40e1b997b0de31
Leonardo Arcari <leonardo1.arcari@gmail.com>
2023-01-19 18:48:12 +0100

    Mobile menu

--- dcapal-frontend/src/components/core/navBar.js
+++ dcapal-frontend/src/components/core/navBar.js
@@ -1,13 +1,72 @@
 import { useMediaQuery } from "@react-hook/media-query";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { MEDIA_SMALL } from "../../app/config";
 import { ExportBtn } from "../exportBtn";
 
+import MenuIcon from "../../../images/icons/hamburger-menu.svg";
+import CloseIcon from "../../../images/icons/close-menu.svg";
+import classNames from "classnames";
+
+const CloseBtn = ({ onClick }) => {
+  return (
+    <div className="cursor-pointer" onClick={onClick}>
+      <img src={CloseIcon} className="w-full max-w-[32px]" />
+    </div>
+  );
+};
+
+const MobileMenu = ({ visible, onClickTitle, toggleMenu }) => {
+  const className = classNames(
+    "absolute z-50 w-screen h-screen inset-0 flex flex-col bg-[#333333]",
+    {
+      invisible: !visible,
+    }
+  );
+
+  return (
+    <div className={className}>
+      <div className="w-full h-14 min-h-[3.5rem] px-4 py-2 flex justify-between items-center bg-[#333333]">
+        <div className="flex gap-x-8">
+          <div
+            className="text-xl font-semibold text-white"
+            onClick={onClickTitle}
+          >
+            <Link to={"/"}>DcaPal</Link>
+          </div>
+        </div>
+        <CloseBtn onClick={toggleMenu} />
+      </div>
+      <div className="flex flex-col px-8 py-3 gap-y-6">
+        <Link to={"/about"} onClick={toggleMenu}>
+          <div className="w-full text-2xl font-light text-white">About</div>
+        </Link>
+        <Link to={"/docs"} onClick={toggleMenu}>
+          <div className="w-full text-2xl font-light text-white">Docs</div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const MenuBtn = ({ onClick }) => {
+  return (
+    <div className="cursor-pointer" onClick={onClick}>
+      <img src={MenuIcon} className="w-full max-w-[32px]" />
+    </div>
+  );
+};
+
 export const NavBar = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
   const navigate = useNavigate();
   const isMobile = !useMediaQuery(MEDIA_SMALL);
+
+  const toggleMenuVisible = () => {
+    setMenuVisible(!menuVisible);
+  };
 
   const onClickHome = () => {
     navigate("/");
@@ -29,8 +88,18 @@ export const NavBar = () => {
             </div>
           </div>
         )}
+        {isMobile && (
+          <MobileMenu
+            visible={menuVisible}
+            toggleMenu={toggleMenuVisible}
+            onClickTitle={onClickHome}
+          />
+        )}
       </div>
-      <ExportBtn />
+      <div className="flex gap-x-2 items-center">
+        <ExportBtn />
+        {isMobile && <MenuBtn onClick={toggleMenuVisible} />}
+      </div>
     </div>
   );
 };
