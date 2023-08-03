e6871f8c5b73ea633ab30c8dc15638adafa26b40
Leonardo Arcari <leonardo1.arcari@gmail.com>
2023-08-03 17:23:51 +0200

    OPT: Check expectations in `test_runner`

---
+++ dcapal-optimizer-wasm/tests/test_runner/main.rs
@@ -0,0 +1,102 @@
+mod error;
+mod scenario;
+
+#[macro_use]
+mod utils_macros;
+
+use dcapal_optimizer_wasm::{
+    optimize::advanced::{self, Solution},
+    JsAdvancedOptions, JsProblemOptions,
+};
+
+use glob::glob;
+use log::info;
+use std::{fs::File, io::BufReader, path::Path};
+
+use error::{Error, Result};
+use scenario::{Expect, ExpectedSolution, Scenario};
+
+const SCENARIOS_PATH: &str = "./tests/scenarios";
+
+#[test_log::test]
+fn test_runner() -> anyhow::Result<()> {
+    info!("==> 🚀  DcaPal Test Runner - Engine ignited");
+
+    let path = Path::new(SCENARIOS_PATH).canonicalize()?;
+    let pattern = format!("{}/**/*.json", SCENARIOS_PATH);
+    info!("==> ⚙️  Loading test scenarios from \"{}\"", path.display());
+
+    let scenarios = glob(&pattern)?.filter_map(|e| e.ok()).collect::<Vec<_>>();
+    for path in &scenarios {
+        info!("==> 💿  Running scenario {:?}", path.file_name().unwrap());
+
+        let scenario = read_scenario_from_file(path)?;
+        let (options, expect) = scenario.split();
+
+        // Build problem from options and solve it
+        let res = match options {
+            JsProblemOptions::Advanced(o) => build_solve_advanced(o),
+            JsProblemOptions::Basic(_) => todo!(),
+        };
+
+        info!("==> 🔬  Checking expectations");
+
+        if matches!(expect, Expect::BuildError) {
+            assert!(res.is_err(), "expect={expect:?} res={res:?}");
+        } else {
+            assert!(res.is_ok(), "expect={expect:?} res={res:?}");
+        }
+
+        match (expect, res.unwrap()) {
+            (Expect::Solved(expected), TestSolution::Advanced(sol)) => {
+                check_expect_advanced(&expected, &sol)
+            }
+            (Expect::Solved(_), TestSolution::Basic) => todo!(),
+            (Expect::BuildError, _) => unreachable!(),
+        }
+    }
+
+    Ok(())
+}
+
+fn read_scenario_from_file(path: &Path) -> anyhow::Result<Scenario> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let scenario: Scenario = serde_json::from_reader(reader)?;
+    Ok(scenario)
+}
+
+fn build_solve_advanced(options: JsAdvancedOptions) -> Result<TestSolution> {
+    let options = advanced::ProblemOptions::try_from(options).map_err(Error::BadProblemInput)?;
+
+    let problem = advanced::Problem::new(options);
+    let solution = problem.solve();
+
+    Ok(TestSolution::Advanced(solution))
+}
+
+fn check_expect_advanced(expected: &ExpectedSolution, sol: &Solution) {
+    assert!(sol.is_solved, "Expected solved problem: sol={sol:?}");
+
+    let Some(ref expected) = expected.solution else { return; };
+
+    for (id, exp) in expected {
+        let asset = sol.assets.get(id);
+        assert!(
+            asset.is_some(),
+            "Asset not found in solution: id={id} sol={sol:?}"
+        );
+        let asset = asset.unwrap();
+
+        asset_equal_field!(id, exp, asset, shares);
+        asset_equal_field!(id, exp, asset, amount);
+        asset_equal_field!(id, exp, asset, weight);
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum TestSolution {
+    Advanced(advanced::Solution),
+    Basic,
+}
---
+++ dcapal-optimizer-wasm/tests/test_runner/scenario.rs
@@ -0,0 +1,123 @@
+use std::collections::BTreeMap;
+
+use dcapal_optimizer_wasm::{JsAdvancedAsset, JsAdvancedOptions, JsProblemOptions};
+use rust_decimal::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Scenario {
+    pub algorithm: Algorithm,
+    #[serde(with = "rust_decimal::serde::float")]
+    pub budget: Decimal,
+    pub is_buy_only: bool,
+    pub portfolio: Portfolio,
+    pub expect: Expect,
+}
+
+impl Scenario {
+    pub fn split(self) -> (JsProblemOptions, Expect) {
+        let expect = self.expect;
+        let options = match self.algorithm {
+            Algorithm::Basic => todo!(),
+            Algorithm::Advanced => {
+                let budget = self.budget.to_f64().unwrap();
+                let pfolio_ccy = self.portfolio.quote_ccy;
+                let is_buy_only = self.is_buy_only;
+                let assets = self
+                    .portfolio
+                    .assets
+                    .into_iter()
+                    .map(|a| (a.symbol.clone(), a.into()))
+                    .collect::<_>();
+
+                JsProblemOptions::Advanced(JsAdvancedOptions {
+                    budget,
+                    pfolio_ccy,
+                    assets,
+                    is_buy_only,
+                })
+            }
+        };
+
+        (options, expect)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum Algorithm {
+    Basic,
+    Advanced,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Portfolio {
+    pub quote_ccy: String,
+    pub assets: Vec<Asset>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Asset {
+    pub symbol: String,
+    pub name: String,
+    pub aclass: AssetClass,
+    #[serde(with = "rust_decimal::serde::float")]
+    pub price: Decimal,
+    #[serde(with = "rust_decimal::serde::float")]
+    pub qty: Decimal,
+    #[serde(with = "rust_decimal::serde::float")]
+    pub target_weight: Decimal,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum AssetClass {
+    Equity,
+    Crypto,
+    Currency,
+}
+
+impl AssetClass {
+    fn is_whole_shares(&self) -> bool {
+        matches!(self, AssetClass::Equity)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "result", rename_all = "camelCase")]
+pub enum Expect {
+    BuildError,
+    Solved(ExpectedSolution),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ExpectedSolution {
+    pub solution: Option<BTreeMap<String, AssetSolution>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AssetSolution {
+    #[serde(with = "rust_decimal::serde::float_option")]
+    pub shares: Option<Decimal>,
+    #[serde(with = "rust_decimal::serde::float_option")]
+    pub amount: Option<Decimal>,
+    #[serde(with = "rust_decimal::serde::float_option")]
+    pub weight: Option<Decimal>,
+}
+
+impl From<Asset> for JsAdvancedAsset {
+    fn from(value: Asset) -> Self {
+        JsAdvancedAsset {
+            symbol: value.symbol,
+            shares: value.qty.to_f64().unwrap(),
+            price: value.price.to_f64().unwrap(),
+            target_weight: value.target_weight.to_f64().unwrap() / 100.,
+            is_whole_shares: value.aclass.is_whole_shares(),
+        }
+    }
+}
