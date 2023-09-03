b48d08681c658c8fd1f773863a4047d6363998fe
Leonardo Arcari <leonardo1.arcari@gmail.com>
2023-09-03 19:10:03 +0200

    FE: Use Webpack's Asset Modules

--- dcapal-frontend/webpack.common.js
+++ dcapal-frontend/webpack.common.js
@@ -29,14 +29,7 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.svg$/,
-          use: [
-            {
-              loader: "svg-url-loader",
-              options: {
-                limit: 10000,
-              },
-            },
-          ],
+          type: "asset/resource",
         },
       ],
     },
