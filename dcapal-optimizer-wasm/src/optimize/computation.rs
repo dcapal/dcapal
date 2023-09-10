2793152762e33bf2f636d6d7e5c1e6308e7164e5
mattia.pitossi <mattia.pitossi@moneyfarm.com>
2023-09-10 16:55:33 +0200

    Provide better variables names

--- dcapal-optimizer-wasm/src/optimize/computation.rs
+++ dcapal-optimizer-wasm/src/optimize/computation.rs
@@ -1,30 +1,6 @@
-use nalgebra::{DMatrix, DVector};
-use rust_decimal::Decimal;
-
 extern crate nalgebra as na;
 
-use na::{Dyn, OMatrix, U1};
-
-
-//to retrieve n I should iterate over the assets and remove those with current_weight > target_weight
-
-fn void() {
-    let mut dm = DMatrix::<f64>::zeros(2, 2);
-    dm.row_mut(0)
-        .copy_from_slice(&[0.8 * 102.72 - 102.72, 27.99 * 0.8]);
-    dm.row_mut(1)
-        .copy_from_slice(&[102.72 * 0.1, 0.1 * 27.99 - 27.99]);
-
-    let mut b = DVector::<f64>::zeros(2);
-    b.copy_from_slice(&[
-        1.00 * 102.72 - 0.8 * 1.00 * 102.72 - 0.8 * 5.00 * 27.99 - 0.8 * 300.00 * 4.56,
-        5.00 * 27.99 - 0.1 * 300.00 * 4.56 - 0.1 * 1.00 * 102.72 - 0.1 * 5.00 * 27.99,
-    ]);
-
-    let decomp = dm.lu();
-    let x = decomp.solve(&b).expect("Linear resolution failed.");
-    println!("Solution: {:?}", x);
-}
+use nalgebra::{DMatrix, DVector};
 
 pub struct ProblemAsset {
     pub symbol: String,
@@ -56,35 +32,46 @@ fn main() {
         current_weight: 0.849,
         target_weight: 0.1,
     };
-    compute_the_amount_to_reach_target_allocation(vec![etf1, etf2, etf3]);
+    calculate_allocation_amount(vec![etf1, etf2, etf3]);
 }
 
-pub fn compute_the_amount_to_reach_target_allocation(pa: Vec<ProblemAsset>) {
-    let (buy, keep): (Vec<_>, Vec<_>) = pa.iter().partition(|a| a.current_weight < a.target_weight);
+pub fn calculate_allocation_amount(assets: Vec<ProblemAsset>) {
+    let (buy, keep): (Vec<_>, Vec<_>) = assets.iter().partition(|a| a.current_weight < a.target_weight);
+
     let n = buy.len();
-    let mut dm = DMatrix::<f64>::zeros(n, n);
-    let mut b = DVector::<f64>::zeros(n);
+    let mut covariance_matrix = DMatrix::<f64>::zeros(n, n);
+    let mut rhs_vector = DVector::<f64>::zeros(n);
 
-    let buy_and_keep: Vec<_> = buy.iter().chain(keep.iter()).cloned().collect();
+    let combined_assets: Vec<_> = buy.iter().chain(keep.iter()).cloned().collect();
 
-    for (i, outer_a) in buy.iter().enumerate() {
-        b[i] = outer_a.price * outer_a.qty;
-        for (j, pa) in buy_and_keep.iter().enumerate() {
-            if pa.current_weight < pa.target_weight {
+    for (i, buy_asset) in buy.iter().enumerate() {
+        // Compute the right-hand side vector 'b'
+        rhs_vector[i] = buy_asset.price * buy_asset.qty;
+
+        for (j, asset) in combined_assets.iter().enumerate() {
+            if asset.current_weight < asset.target_weight {
                 if i == j {
-                    dm[(i, j)] = pa.price * pa.target_weight - pa.price;
+                    // Diagonal elements of the covariance matrix
+                    covariance_matrix[(i, j)] = asset.price * asset.target_weight - asset.price;
                 } else {
-                    dm[(i, j)] = pa.price * outer_a.target_weight;
+                    // Off-diagonal elements of the covariance matrix
+                    covariance_matrix[(i, j)] = asset.price * buy_asset.target_weight;
                 }
             }
-            b[i] -= pa.price * pa.qty * outer_a.target_weight;
+            rhs_vector[i] -= asset.price * asset.qty * buy_asset.target_weight;
         }
     }
-    let decomp = dm.lu();
-    let x = decomp.solve(&b).expect("Linear resolution failed.");
-    println!("Solution: {:?}", x);
-    let sum_product = buy.iter().zip(x.iter()).fold(0.0, |acc, (a, x)| {
-        acc + (a.price * x)
+
+    // Perform LU decomposition and solve for 'x'
+    let decomposition = covariance_matrix.lu();
+    let allocation_amount = decomposition.solve(&rhs_vector).expect("Linear resolution failed.");
+
+    println!("Allocation Amounts: {:?}", allocation_amount);
+
+    // Calculate the total amount needed to invest for reaching the target allocation
+    let sum_product = buy.iter().zip(allocation_amount.iter()).fold(0.0, |acc, (asset, x)| {
+        acc + (asset.price * x)
     });
-    println!("Sum: {}", sum_product);
+
+    println!("Total Investment for 'buy' assets: {}", sum_product);
 }
