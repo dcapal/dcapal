8154bbe310929c1572f61e6703c8ac3d91129477
Corrado Petrelli <corrado.petrelli@gmail.com>
2023-09-12 19:06:52 +0200

    cosmetics 💅

--- dcapal-frontend/src/components/languageSwitcher.js
+++ dcapal-frontend/src/components/languageSwitcher.js
@@ -10,7 +10,9 @@ function LanguageSwitcher() {
   return (
     <select
       className="w-full w-fit px-3 py-2 pb-1.5 border focus-visible:outline-1 rounded-md text-center border-gray-300 hover:border-gray-500 focus-visible:outline-gray-600 leading-normal"
-      value={useSelector((state) => state.app.language) || i18n.resolvedLanguage}
+      value={
+        useSelector((state) => state.app.language) || i18n.resolvedLanguage
+      }
       onChange={(e) => {
         const newLang = e.target.value;
         i18n.changeLanguage(newLang).then(() => {
