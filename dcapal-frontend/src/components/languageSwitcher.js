86182b92339bb576b07b013667071280c9f564b9
Leonardo Arcari <leonardo1.arcari@gmail.com>
2023-09-16 11:11:16 +0200

    FE: Cosmetics on `LanguageSwitcher` component

--- dcapal-frontend/src/components/languageSwitcher.js
+++ dcapal-frontend/src/components/languageSwitcher.js
@@ -9,7 +9,7 @@ function LanguageSwitcher() {
 
   return (
     <select
-      className="w-full w-fit px-3 py-2 pb-1.5 border focus-visible:outline-1 rounded-md text-center border-gray-300 hover:border-gray-500 focus-visible:outline-gray-600 leading-normal"
+      className="pl-3 pr-1.5 py-2 pb-2 border focus-visible:outline-1 rounded-md text-center border-gray-300 hover:border-gray-500 focus-visible:outline-gray-600 leading-normal"
       value={
         useSelector((state) => state.app.language) || i18n.resolvedLanguage
       }
