28c0dd1090511d5cb161bc4b72b1cc402d2b9f50
Leonardo Arcari <leonardo1.arcari@gmail.com>
2023-09-18 11:02:56 +0200

    FE: Use `baseUrl` in Playwright

--- dcapal-frontend/tests/example.spec.js
+++ dcapal-frontend/tests/example.spec.js
@@ -1,9 +1,11 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('has title', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8080/');
+test("has title", async ({ page }) => {
+  await page.goto("/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/DcaPal - A smart assistant for your periodic investments | DcaPal/);
+  await expect(page).toHaveTitle(
+    /DcaPal - A smart assistant for your periodic investments | DcaPal/
+  );
 });
