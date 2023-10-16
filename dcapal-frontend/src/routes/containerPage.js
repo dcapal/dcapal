bc6c2c74e2e94ddedc02afa25356cd4d8b6bc515
Corrado Petrelli <corrado.petrelli@gmail.com>
2023-10-16 19:03:52 +0200

    feat: create component cookie

--- dcapal-frontend/src/routes/containerPage.js
+++ dcapal-frontend/src/routes/containerPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavBar } from "../components/core/navBar";
 import { DcaPalHelmet } from "./helmet";
 import { Footer } from "../components/core/footer";
+import { CookieButton } from "../components/core/cookieButton";
 
 export const ContainerPage = ({ id, title, content }) => {
   return (
@@ -12,22 +13,7 @@ export const ContainerPage = ({ id, title, content }) => {
           <NavBar />
           {content}
           <Footer />
-          <button
-            type="button"
-            style={{
-              position: "fixed",
-              bottom: 0,
-              right: 0,
-              top: "unset",
-              zIndex: 3147483649,
-              backgroundColor: "#ccc",
-              padding: "5px 5px 0",
-              borderTopLeftRadius: "0.5rem",
-            }}
-            data-cc="c-settings"
-          >
-            🍪
-          </button>
+          <CookieButton/>
         </div>
       </div>
     </>
