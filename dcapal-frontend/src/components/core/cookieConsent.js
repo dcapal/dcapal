5c4a263dff16ddcb5c1384b0788fe6c91f73c9ed
Corrado Petrelli <corrado.petrelli@gmail.com>
2023-11-02 22:23:43 +0100

    cosmetics

--- dcapal-frontend/src/components/core/cookieConsent.js
+++ dcapal-frontend/src/components/core/cookieConsent.js
@@ -1,18 +1,16 @@
 import { useEffect } from "react";
 
-import 'vanilla-cookieconsent';
-import 'vanilla-cookieconsent/dist/cookieconsent.css';
+import "vanilla-cookieconsent";
+import "vanilla-cookieconsent/dist/cookieconsent.css";
 import pluginConfig from "./cookieConfig";
 
 export default function CookieConsent() {
-    useEffect(() => {
+  useEffect(() => {
+    if (!document.getElementById("cc--main")) {
+      window.CC = window.initCookieConsent();
+      window.CC.run(pluginConfig);
+    }
+  }, []);
 
-        if (!document.getElementById('cc--main')) {
-            window.CC = window.initCookieConsent();
-            window.CC.run(pluginConfig);
-        }
-
-    }, []);
-
-    return null;
+  return null;
 }
