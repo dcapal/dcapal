aa41e5cf4d49a6947d82a6307571c2859141439c
pietro grandi <pietro.grandi@casavo.com>
2024-03-10 13:45:40 +0100

    feat: restore i18n

--- dcapal-frontend/src/main.tsx
+++ dcapal-frontend/src/main.tsx
@@ -6,8 +6,8 @@ import "./style.css";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import translationEN from "../public/locales/en/translation.json";
-import translationIT from "../public/locales/it/translation.json";
+import translationEN from "/locales/en/translation.json";
+import translationIT from "/locales/it/translation.json";
 
 i18n
   .use(LanguageDetector)
