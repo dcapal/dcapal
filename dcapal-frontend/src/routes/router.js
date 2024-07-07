39ea277d0644aef21d6ad7119652d1cdba533ff5
Mattia Pitossi <mattia.pitossi@moneyfarm.com>
2024-07-07 12:23:06 +0200

    Update project repo references

--- dcapal-frontend/src/routes/router.js
+++ dcapal-frontend/src/routes/router.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRoutes } from "react-router-dom";
 import { App } from "@app";
 
@@ -13,13 +13,28 @@ import UnderConstructionPage from "./underConstruction";
 import {
   DEMO_PF_60_40,
   DEMO_PF_ALL_SEASONS,
-  DEMO_PF_MR_RIP,
   DEMO_PF_HODLX,
+  DEMO_PF_MR_RIP,
+  supabase,
 } from "@app/config";
-import Auth from "@routes/loginPage";
 import AuthPage from "@routes/loginPage";
+import Account from "@routes/profilePage";
 
 export const Router = () => {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
   let routesConfig = [
     {
       path: "*",
@@ -53,6 +68,19 @@ export const Router = () => {
     {
       path: "login",
       element: <AuthPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "profile",
+      element: (
+        <div className="container" style={{ padding: "50px 0 100px 0" }}>
+          {!session ? (
+            <AuthPage />
+          ) : (
+            <Account key={session.user.id} session={session} />
+          )}
+        </div>
+      ),
       errorElement: <ErrorPage />,
     },
   ];
