85577153908b9627c9becd67ad51291f7207514b
Leonardo Arcari <leonardo1.arcari@gmail.com>
2024-07-20 17:08:38 +0200

    build(deps): Bump JS dependencies

--- dcapal-frontend/webpack.dev.js
+++ dcapal-frontend/webpack.dev.js
@@ -16,23 +16,26 @@ module.exports = (env, argv) =>
       port: 3000,
       historyApiFallback: true,
       hot: true,
-      proxy: {
-        "/api/external/chart": {
+      proxy: [
+        {
+          context: ["/api/external/chart"],
           target: "https://query1.finance.yahoo.com/v8/finance/chart/",
           pathRewrite: { "^/api/external/chart": "" },
           changeOrigin: true,
         },
-        "/api/external/search": {
+        {
+          context: ["/api/external/search"],
           target: "https://query2.finance.yahoo.com/v1/finance/search",
           pathRewrite: { "^/api/external/search": "" },
           changeOrigin: true,
         },
-        "/api": {
+        {
+          context: ["/api"],
           target: "http://0.0.0.0:8080",
           pathRewrite: { "^/api": "" },
           changeOrigin: true,
         },
-      },
+      ],
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "*",
