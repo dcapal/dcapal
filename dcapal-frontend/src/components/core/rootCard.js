d9379eaeb4dd5f8e9ff397cd1435f24a14238e23
Mattia Pitossi <mattia.pitossi@moneyfarm.com>
2024-07-20 16:04:45 +0200

    Fix icons on landing

--- dcapal-frontend/src/components/core/rootCard.js
+++ dcapal-frontend/src/components/core/rootCard.js
@@ -4,14 +4,17 @@ export const RootCard = ({ id, imgSrc, text, ...props }) => {
   return (
     <div
       id={id}
-      className="w-full min-h-[11.5rem] max-h-[11.5rem] flex flex-col gap-y-2 items-center justify-center bg-[#F3F4F6] rounded"
+      className="bg-card p-6 rounded-lg shadow-md bg-[#F3F4F6] rounded text-center"
     >
-      <img
-        alt="Icon homepage"
-        className="w-full max-w-[4rem] p-1 rounded-full bg-neutral-600"
-        src={imgSrc}
-      />
-      <div className="w-4/5 text-xl text-center font-light">{text}</div>
+      <div className="flex justify-center items-center">
+        <img
+          alt="Icon homepage"
+          className="w-full max-w-[4rem] p-1 rounded-full bg-neutral-600"
+          src={imgSrc}
+        />
+      </div>
+      <p className="text-xl font-light mt-2">{text}</p>
+      <div className="w-4/5 mx-auto text-center font-light"></div>
     </div>
   );
 };
