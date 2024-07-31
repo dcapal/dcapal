64842ba4c0643e3def4ff4bfcfc2a5bc504f4007
Mattia Pitossi <mattia.pitossi@moneyfarm.com>
2024-07-31 16:43:44 +0200

    Add Postgres DB support

--- dcapal-backend/migrations/20240731073355_init.sql
+++ dcapal-backend/migrations/20240731073355_init.sql
@@ -2,7 +2,7 @@
 create table public.users (
   id          uuid references auth.users not null primary key, -- UUID from auth.users
   username    text,
-  status      user_status default 'OFFLINE'::public.user_status
+  email text not null unique
 );
 comment on table public.users is 'Profile data for each user.';
 comment on column public.users.id is 'References the internal Supabase Auth user.';
