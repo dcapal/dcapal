60530a0d34a15bf69774c8d3e7beaa93cab98374
Leonardo Arcari <leonardo1.arcari@gmail.com>
2024-08-25 22:44:31 +0200

    fix(frontend): improve LanguageSwitcher a11y

--- dcapal-frontend/src/components/languageSwitcher.js
+++ dcapal-frontend/src/components/languageSwitcher.js
@@ -7,6 +7,7 @@ function LanguageSwitcher() {
   return (
     <select
       className="pl-3 pr-1.5 py-2 border focus-visible:outline-1 rounded-md text-center border-gray-300 hover:border-gray-500 focus-visible:outline-gray-600 leading-normal"
+      aria-label="Change language"
       value={i18n.resolvedLanguage}
       onChange={(e) => {
         const newLang = e.target.value;
