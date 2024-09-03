63f30ecf79c10a65e4c3de57dc4c7f53aed17e25
Mattia Pitossi <mattia.pitossi@moneyfarm.com>
2024-09-03 13:18:27 +0200

    Allow wrong message for unused imports

--- dcapal-backend/src/ports/outbound/repository/ai.rs
+++ dcapal-backend/src/ports/outbound/repository/ai.rs
@@ -1,3 +1,4 @@
+#[allow(unused_imports)]
 use crate::app::domain::entity::{
     Ai, InvestmentGoal, InvestmentMode, InvestmentPreferences, Portfolio, RiskTolerance,
 };
--- dcapal-backend/src/ports/outbound/repository/user.rs
+++ dcapal-backend/src/ports/outbound/repository/user.rs
@@ -1,3 +1,4 @@
+#[allow(unused_imports)]
 use crate::app::domain::entity::{
     InvestmentGoal, InvestmentMode, InvestmentPreferences, RiskTolerance, User,
 };
