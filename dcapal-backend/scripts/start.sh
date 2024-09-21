1335a3a8eee5db649562ddcd1bb4fccc49e70ed0
Leonardo Arcari <leonardo1.arcari@gmail.com>
2024-09-21 15:11:56 +0200

    ops(be): fix start.sh postgres host

--- dcapal-backend/scripts/start.sh
+++ dcapal-backend/scripts/start.sh
@@ -3,8 +3,6 @@ set -e
 
 # Check if a custom parameter has been set, otherwise use default values
 DB_PORT="${DB_PORT:=5432}"
-SUPERUSER="${SUPERUSER:=postgres}"
-SUPERUSER_PWD="${SUPERUSER_PWD:=postgres}"
 APP_USER="${APP_USER:=postgres}"
 APP_USER_PWD="${APP_USER_PWD:=postgres}"
 APP_DB_NAME="${APP_DB_NAME:=postgres}"
@@ -19,7 +17,7 @@ done
 echo >&2 "Postgres is up - executing command"
 
 # Create the application database
-DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@postgres-db:${DB_PORT}/${APP_DB_NAME}
+DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@${$DB_HOST}:${DB_PORT}/${APP_DB_NAME}
 export DATABASE_URL
 # Run migrations
 sqlx migrate run
