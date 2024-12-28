25d09eb8a8f11f8d311988cb656a03fec9644c66
Leonardo Arcari <leonardo1.arcari@gmail.com>
2024-12-28 16:16:17 +0100

    feat: Button variants improvement

--- dcapal-frontend/src/components/ui/button.jsx
+++ dcapal-frontend/src/components/ui/button.jsx
@@ -13,17 +13,18 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-foreground font-medium underline-offset-4 hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        link: "h-10 px-0 py-2",
       },
     },
     defaultVariants: {
