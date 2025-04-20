273d6d76f3f70373d1c405e0676d8bed40e532c9
Mattia Pitossi <mattia.pitossi@moneyfarm.com>
2025-04-20 15:50:04 +0200

    feat: improve the allocation flow UX

--- dcapal-frontend/src/routes/aboutPage.js
+++ dcapal-frontend/src/routes/aboutPage.js
@@ -138,6 +138,7 @@ export default function AboutPage() {
                   components={[
                     <span className="font-normal" />,
                     <a className="underline" href="#social-profiles" />,
+                    <a href="mailto:support@dcapal.com" />
                   ]}
                 />
               </p>
