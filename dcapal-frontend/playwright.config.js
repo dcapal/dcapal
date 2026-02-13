9dea87f698c467db10a6ce6e026a9d53797d4a1a
leonardoarcari <leonardo1.arcari@gmail.com>
2026-02-14 00:01:05 +0100

    ci: fix playwright webserver

--- dcapal-frontend/playwright.config.js
+++ dcapal-frontend/playwright.config.js
@@ -71,12 +71,8 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run start",
+    command: "REACT_APP_E2E_MSW=1 npm run start:ci",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
-    env: {
-      ...process.env,
-      REACT_APP_E2E_MSW: "1",
-    },
   },
 });
